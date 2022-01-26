3f5c972495850e3b657badfbeeae190079b2da66
Starzbots <96718781+Starzbots@users.noreply.github.com>
2022-01-26 19:07:48 +0530

    Update config.py

--- Noinoi/config.py
+++ Noinoi/config.py
@@ -8,11 +8,11 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "AQCbBG5IUa0Oh_r-h5kAcupsPQUefNMmH7hzrWppaMaZWpzN-x3DTylBpKipbr2dHjd8CltY6ooNBLOTOncZFJkjAfRV3ggN0bfN_sn9-emnY6JOYrq2lw2SkzQZVj_bwDjr3FtwAy6NTiiFGyYU50IsnE8Oqv_rnXLL_sQKrvNCBbxdDOiuXizePLEyZTg6mr75HSXX51UCO41SmLqQnSRs7fEK8bwn6-yMXqOx-1ObmNiSFZZDYOnp2PKHhyfmatawXWpeC42r7FCe9oCHGPrF_cO4OeViV5h5P8gSVCL5heauh917LNsVV2IwVSqqk9uTWu6CRVzKco5ON-ScOrG-AAAAASrnbxEA")
-BOT_TOKEN = getenv("BOT_TOKEN", "5007724504:AAFm7cZ50CRv0FjDZmaZ77Fh6YsxoW83Tw0")
+SESSION_NAME = getenv("SESSION_NAME", "chootAQCbBG5IUa0Oh_cupsPQUefNMmH7hzrWppaMaZWpzN-x3DTylBpKipbr2dHjd8CltY6ooNBLOTOncZFJkjAfRV3ggN0bfN_sn9-emnY6JOYrq2lw2SkzQZVj_bwDjr3FtwAy6NTiiFGyYU50IsnE8Oqv_rnXLL_sQKrvNCBbxdDOiuXizePLEyZTg6mr75HSXX51UCO41SmLqQnSRs7fEK8bwn6-yMXqOx-1ObmNiSFZZDYOnp2PKHhyfmatawXWpeC42r7FCe9oCHGPrF_cO4OeViV5h5P8gSVCL5heauh917LNsVV2IwVSqqk9uTWu6CRVzKco5ON-ScOrG-AAAAASrnbxEA")
+BOT_TOKEN = getenv("BOT_TOKEN", "lund0CRv0FjDZmaZ77Fh6YsxoW83Tw0")
 BOT_NAME = getenv("BOT_NAME", " Alexa bot")
 API_ID = int(getenv("API_ID", "9273779"))
-API_HASH = getenv("API_HASH", "287aab6efb13cdd2f042232e704d26ce")
+API_HASH = getenv("API_HASH", "lawda287aab6efb13cdd2f042232e704d26ce")
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/5b395a5e54aee892814dc.jpg")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/ce5c01d04da27473b5b42.jpg")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
